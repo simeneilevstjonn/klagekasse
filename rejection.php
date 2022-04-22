14320067f1e7ba036094ac5d55c26dffe7d85473
Simen Eilevstjønn <simene@lyse.net>
2022-04-22 18:30:02 +0200

    Dummy rejection mail

---
+++ rejection.php
@@ -0,0 +1,30 @@
+<!doctype HTML>
+<html>
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+<p>
+    Dear customer
+</p>
+
+<p>
+    We are referring to your inquiry with ID <strong><?php echo $argv[1]; ?></strong>.
+</p>
+
+<p>
+    We have evaluated your inquiry, and found that it is not applicable.
+</p>
+
+<p>
+    Your inquiry has been closed.<br>
+    Please open a new inquiry if you still need support.
+</p>
+
+<p>
+    Best regards,<br>
+    <br>
+    Company Name
+</p>
+</body>
+</html>
