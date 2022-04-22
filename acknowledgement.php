8e3ec0a9805df62e071f596d73d50a1fdf913faf
Simen Eilevstjønn <simene@lyse.net>
2022-04-22 16:50:19 +0200

    Dummy acknowledgement file in PHP

---
+++ acknowledgement.php
@@ -0,0 +1,23 @@
+<!doctype HTML>
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <p>
+            Thank you for your inquiry.
+        </p>
+
+        <p>
+            Your case has been assigned case ID <strong>IDXXXXX</strong>.<br>
+            Please refer to this ID for all further communication.<br>
+            We will get back to you within five business days.
+        </p>
+
+        <p>
+            Best regards,<br>
+            <br>
+            Company Name
+        </p>
+    </body>
+</html>
